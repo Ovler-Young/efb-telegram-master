d3f88d5cec8a0f20e815320cd7e17aedee7de014
Ovler <ovlertheyoung@gmail.com>
2025-08-04 10:41:32 +0800

    chore: add more sending margin

--- efb_telegram_master/bot_manager.py
+++ efb_telegram_master/bot_manager.py
@@ -332,13 +332,13 @@ class TelegramBotManager(LocaleMixin):
             self._cleanup_old_timestamps()
 
             # global rate limit
-            if len(self._global_timestamps) >= self.GLOBAL_LIMIT - 1:
-                sleep_time = max(sleep_time, self.GLOBAL_WINDOW - (current_time - self._global_timestamps[0]))
+            if len(self._global_timestamps) >= self.GLOBAL_LIMIT - 2:
+                sleep_time = max(sleep_time, self.GLOBAL_WINDOW - (current_time - self._global_timestamps[0]) + 1)
 
             # chat-specific rate limit
             chat_timestamps = self._chat_timestamps[chat_id]
-            if len(chat_timestamps) >= self.CHAT_LIMIT - 1:
-                sleep_time = max(sleep_time, self.CHAT_WINDOW - (current_time - chat_timestamps[0]))
+            if len(chat_timestamps) >= self.CHAT_LIMIT - 2:
+                sleep_time = max(sleep_time, self.CHAT_WINDOW - (current_time - chat_timestamps[0]) + 1)
             # Record the actual time this request will be processed
             actual_time = current_time + sleep_time
             self._global_timestamps.append(actual_time)
