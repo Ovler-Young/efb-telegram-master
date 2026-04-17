44bf1605bafd49b58c7b4cbd3ace7862dad24a4a
Ovler <ovlertheyoung@gmail.com>
2026-04-17 15:10:23 -0400

    fix: type

--- tests/integration/test_master_message_not_supported.py
+++ tests/integration/test_master_message_not_supported.py
@@ -7,7 +7,7 @@ from pytest import mark
 from telethon.tl.custom import Message, InlineResults
 from telethon.tl.functions.messages import GetStickerSetRequest, GetInlineBotResultsRequest
 from telethon.tl.types import InputStickerSetAnimatedEmoji, Document, PollAnswer, \
-    InputMediaPoll, Poll
+    InputMediaPoll, Poll, TextWithEntities
 from telethon.tl.types.messages import StickerSet
 
 from .utils import link_chats
@@ -56,8 +56,8 @@ def _build_poll(question: str, *answers: str, closed: Optional[bool] = None,
     https://github.com/pyrogram/pyrogram/blob/develop/pyrogram/client/methods/messages/send_poll.py
     https://github.com/pyrogram/pyrogram/blob/develop/pyrogram/client/methods/messages/stop_poll.py"""
     return InputMediaPoll(Poll(
-        id=id, question=question, answers=[
-            PollAnswer(text=i, option=bytes([idx]))
+        id=id, question=TextWithEntities(text=question, entities=[]), answers=[
+            PollAnswer(text=TextWithEntities(text=i, entities=[]), option=bytes([idx]))
             for idx, i in enumerate(answers)
         ],
         closed=closed,
